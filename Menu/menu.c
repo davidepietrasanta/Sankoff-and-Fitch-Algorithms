0e3cbc3444e50f8af89222aa73d7497973ebc2bc
Davide Pietrasanta <davide.pietrasanta@virgilio.it>
2020-03-06 18:08:51 +0100

    Sankoff added to Makefile and to Menu

--- Menu/menu.c
+++ Menu/menu.c
@@ -3,6 +3,7 @@
 #include <stdbool.h> 
 #include <stdlib.h>
 #include "Fitch.h"
+#include "Sankoff.h"
 
 
 void caseFitch(){
@@ -13,6 +14,37 @@ void caseFitch(){
     printf("Insert the path in witch there is the Newick Tree\n");
     printf("The path should not be greater than 10000\n");
     scanf("%s",&*path);
+    fitch_algorithm(path);
+}
+
+void caseSankoff(){
+    /*Si occupa di prendere in input il path*/
+    
+    printf("Sankoff Algorithm\n");
+    char path[10000];
+    char pathM[10000];
+    printf("Insert the path in witch there is the Newick Tree\n");
+    printf("The path should not be greater than 10000\n");
+    scanf("%s",&*path);
+    printf("Insert the path in witch there is the Matrix\n");
+    printf("The path should not be greater than 10000\n");
+    scanf("%s",&*pathM);
+    sankoff_algorithm(path, pathM);
+}
+
+void caseBoth(){
+    /*Si occupa di prendere in input il path*/
+
+    printf("Sankoff and Fitch Algorithms\n");
+    char path[10000];
+    char pathM[10000];
+    printf("Insert the path in witch there is the Newick Tree\n");
+    printf("The path should not be greater than 10000\n");
+    scanf("%s",&*path);
+    printf("Insert the path in witch there is the Matrix\n");
+    printf("The path should not be greater than 10000\n");
+    scanf("%s",&*pathM);
+    sankoff_algorithm(path, pathM);
     fitch_algorithm(path);
 }
 
@@ -36,10 +68,10 @@ void menu(){
     //clrscr();
     switch(input){
         case '0' : 
-            printf("Both\n"); 
+            caseBoth();
             break;
         case '1' : 
-            printf("Sankoff\n"); 
+            caseSankoff(); 
             break;
         case '2' : 
             caseFitch();
